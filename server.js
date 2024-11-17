9db4650f47e9e3c2b145a4dd4895ab2138350909
ViktorIRed <devacephone6@gmail.com>
2024-11-17 03:44:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/");
+const mongooseURL = "mongodb://localhost:27017/"
+mongoose.connect(mongooseURL);
 const express = require("express");
 const { router } = require("./router");
 const path = require("path");
